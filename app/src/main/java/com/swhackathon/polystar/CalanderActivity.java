dc38d6dd58c25d3c3977f2d1cba844c55bd0b141
chea yeon <cna3762@gmail.com>
2021-02-05 02:52:41 +0900

    달력으로 이동하는 것 하지만 잘 작동되지 않는다

--- app/src/main/java/com/swhackathon/polystar/CalanderActivity.java
+++ app/src/main/java/com/swhackathon/polystar/CalanderActivity.java
@@ -1,4 +1,75 @@
 package com.swhackathon.polystar;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class CalanderActivity {
+public class CalanderActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calander);
+
+
+        //타이틀바 삭제 시작
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        //타이틀바 삭제 종료
+
+        //네비게이션 바 버튼
+        //홈버튼
+        ImageButton home = (ImageButton) findViewById(R.id.homeButton1);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //별의 기억버튼
+        ImageButton memory = (ImageButton) findViewById(R.id.memoryButton1);
+        memory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CalanderActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //별자리버튼
+        ImageButton star = (ImageButton) findViewById(R.id.starButton1);
+        star.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //즐겨찾기버튼
+        ImageButton like = (ImageButton) findViewById(R.id.likeButton1);
+        like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //뒤로가기 버튼
+        ImageButton back = (ImageButton) findViewById(R.id.backButton);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
 }
+
--- app/src/main/java/com/swhackathon/polystar/InputActivity.java
+++ app/src/main/java/com/swhackathon/polystar/InputActivity.java
@@ -15,5 +15,57 @@ public class InputActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
         //타이틀바 삭제 종료
+
+        //네비게이션 바 버튼
+        //홈버튼
+        ImageButton home = (ImageButton) findViewById(R.id.homeButton);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //별의 기억버튼
+        ImageButton memory = (ImageButton) findViewById(R.id.memoryButton);
+        memory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CalanderActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //별자리버튼
+        ImageButton star = (ImageButton) findViewById(R.id.starButton);
+        star.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //즐겨찾기버튼
+        ImageButton like = (ImageButton) findViewById(R.id.likeButton);
+        like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //뒤로가기 버튼
+        ImageButton back = (ImageButton) findViewById(R.id.backButton);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
