ddcfd57fec8870f963e70e9e957ee6ac5dd667bd
Hyunho Kim <void_laboratory@naver.com>
2021-02-05 04:42:00 +0900

    가이드 마지막 작업완료

---
+++ app/src/main/java/com/swhackathon/polystar/StarQuestionActivity4.java
@@ -0,0 +1,14 @@
+package com.swhackathon.polystar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StarQuestionActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_star_question4);
+    }
+}
