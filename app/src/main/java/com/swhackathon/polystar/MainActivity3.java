e2a13b8bdd77f166325bf3279c8e49b7af11f0c5
Hyunho Kim <void_laboratory@naver.com>
2021-02-05 20:30:10 +0900

    다시 시도

--- app/src/main/java/com/swhackathon/polystar/MainActivity3.java
+++ app/src/main/java/com/swhackathon/polystar/MainActivity3.java
@@ -2,7 +2,20 @@ package com.swhackathon.polystar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -10,5 +23,81 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        //날짜 출력
+        Date currentTime = Calendar.getInstance().getTime();
+        String date = new SimpleDateFormat("MM月 / dd日", Locale.getDefault()).format(currentTime);
+
+        TextView textView = findViewById(R.id.date);
+
+        textView.setText(date);
+
+        //네비게이션 바 버튼
+        //홈버튼
+        ImageButton home = (ImageButton) findViewById(R.id.homeButton);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+                startActivity(intent);
+
+            }
+        });
+
+        //별의 기억버튼
+        ImageButton memory = (ImageButton) findViewById(R.id.memoryButton);
+        memory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CalanderActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //별자리버튼
+        ImageButton star = (ImageButton) findViewById(R.id.starButton);
+        star.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), StarActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //즐겨찾기버튼
+        ImageButton like = (ImageButton) findViewById(R.id.likeButton);
+        like.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), BookmarkActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //질문 버튼 (달)
+        ImageButton moon = (ImageButton) findViewById(R.id.moonButton);
+        moon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), InputActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 임시용 로그아웃 버튼
+        Button logout = (Button)findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity3.this, JoinActivity.class);
+                startActivity(intent);
+                SharedPreferences auto = getSharedPreferences("auto", Activity.MODE_PRIVATE);
+                SharedPreferences.Editor editor = auto.edit();
+                editor.clear();
+                editor.commit();
+                Toast.makeText(MainActivity3.this, "로그아웃.", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
     }
 }
