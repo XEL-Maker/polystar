b39420963d8c65fdc50cae302059e0f4e8505996
eksrude <eksrud0099@cu.ac.kr>
2021-02-05 16:38:28 +0900

    x

--- app/src/main/java/com/swhackathon/polystar/JoinActivity.java
+++ app/src/main/java/com/swhackathon/polystar/JoinActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,8 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 
 public class JoinActivity extends AppCompatActivity {
@@ -99,8 +102,13 @@ public class JoinActivity extends AppCompatActivity {
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+
             }
+
+
         }
+
+
     }
 
 
