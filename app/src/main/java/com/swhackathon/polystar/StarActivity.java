03667fff07218c1d431f88cb9004758d20aa35da
Taeyeon Kang <ka704025@gmail.com>
2021-02-05 16:11:25 +0900

    사진 선택 코드 (별자리 페이지) 복사 완료

--- app/src/main/java/com/swhackathon/polystar/StarActivity.java
+++ app/src/main/java/com/swhackathon/polystar/StarActivity.java
@@ -5,18 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.io.InputStream;
+
 public class StarActivity extends AppCompatActivity {
 
     String loginName, loginId;
+
+    ImageButton imageEdit;
+    ImageView imageProfile;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -84,5 +92,40 @@ public class StarActivity extends AppCompatActivity {
 
         textName.setText("이름: " + loginName);
         textId.setText("닉네임: " + loginId);
+
+        // 갤러리 구현
+        imageProfile = (ImageView) findViewById(R.id.imageView18);
+        imageEdit = (ImageButton) findViewById(R.id.imageButton9);
+
+        imageEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override   //갤러리 들어가는 코드
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                try {
+                    // 선택한 이미지에서 비트맵 생성
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+                    // 이미지 표시
+                    imageProfile.setImageBitmap(img);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
