90483d78289e9bdb12b0a9622528bfcc4276238e
Hyunho Kim <void_laboratory@naver.com>
2021-02-05 20:47:38 +0900

    3개중 2개 연결완료

--- app/src/main/java/com/swhackathon/polystar/MainActivity4.java
+++ app/src/main/java/com/swhackathon/polystar/MainActivity4.java
@@ -2,27 +2,23 @@ package com.swhackathon.polystar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
-public class MainActivity3 extends AppCompatActivity {
+public class MainActivity4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+        setContentView(R.layout.activity_main4);
         //날짜 출력
         Date currentTime = Calendar.getInstance().getTime();
         String date = new SimpleDateFormat("MM月 / dd日", Locale.getDefault()).format(currentTime);
@@ -37,7 +33,7 @@ public class MainActivity3 extends AppCompatActivity {
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity4.class);
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
 
             }
