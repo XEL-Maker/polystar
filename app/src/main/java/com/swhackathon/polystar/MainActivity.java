6685bfc2d443899fb05b296ad799d0cf15e1ff60
Hyunho Kim <void_laboratory@naver.com>
2021-02-05 11:17:40 +0900

    즐겨찾기 작업중

--- app/src/main/java/com/swhackathon/polystar/MainActivity.java
+++ app/src/main/java/com/swhackathon/polystar/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         like.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                Intent intent = new Intent(getApplicationContext(), BookmarkActivity.class);
                 startActivity(intent);
             }
         });
